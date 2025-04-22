6c2b44c8b1e671e0be7f6a99869f5db8c4743f4b
Guilherme Batista <capixth@gmail.com>
2025-04-21 22:12:43 -0300

    Contador

--- Portifolio/src/App.jsx
+++ Portifolio/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [valor, setValor] = useState(0);
+
+  const add = () => {
+    setValor(valor + 1);
+  };
+
+  const reduce = () => {
+    if (valor>0) {
+      setValor(valor-1);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Primeiro Contador</h1>
+      <div className="m-2 fs-3">{valor}</div>
+      <div className="btn btn-primary" onClick={reduce}>Reduzir</div>
+      <div className="btn btn-primary" onClick={add}>
+        Adicionar
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
