97e49e7688a8d077017a3f0cfe3b2d5f7c44e6a6
Guilherme Batista <capixth@gmail.com>
2025-04-24 20:04:12 -0300

    create: Montando a NavBar do Portifolio

--- Portifolio/src/App.jsx
+++ Portifolio/src/App.jsx
@@ -1,31 +1,21 @@
-import { useState } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import {
+  CNavbar,
+  CContainer,
+  CNavbarBrand,
+} from '@coreui/react';
+import '@coreui/coreui/dist/css/coreui.min.css';
 
-const App = () => {
-
-  const [valor, setValor] = useState(0);
-
-  const add = () => {
-    setValor(valor + 1);
-  };
-
-  const reduce = () => {
-    if (valor>0) {
-      setValor(valor-1);
-    }
-  };
-
+const NavBar = () => {
   return (
-    <div>
-      <h1>Primeiro Contador</h1>
-      <div className="m-2 fs-3">{valor}</div>
-      <div className="btn btn-primary" onClick={reduce}>Reduzir</div>
-      <div className="btn btn-primary" onClick={add}>
-        Adicionar
-      </div>
-    </div>
+    <>
+      <CNavbar expand="xxl" className="bg-body-tertiary">
+        <CContainer fluid>
+          <CNavbarBrand href="#">Guilherme Batista</CNavbarBrand>
+        </CContainer>
+      </CNavbar>
+    </>
   );
 };
 
-export default App;
+export default NavBar;
