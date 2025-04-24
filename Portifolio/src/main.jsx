c7c2b8f5d42283c90a39f09631ba6e2fd821d034
Guilherme Batista <capixth@gmail.com>
2025-04-24 20:13:30 -0300

    fix: Ajustando caminhos das pastas de NavBar

--- Portifolio/src/main.jsx
+++ Portifolio/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/NavBar/NavBar.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="NavBar" id="NavBar">
+      <App />
+    </div>
+  </StrictMode>
+);
