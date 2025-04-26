5a8cc74b440208dd46852d59eee98cfde5a2b9a4
Guilherme Batista <capixth@gmail.com>
2025-04-25 22:09:38 -0300

    fix: Ajustando caminho das pastas

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './components/NavBar/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="NavBar" id="NavBar">
-      <App />
+      <NavBar />
     </div>
   </StrictMode>
 );
