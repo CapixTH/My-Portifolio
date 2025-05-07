8a666313ac3100d79885f9725630d39f32dd70e3
Guilherme Batista <capixth@gmail.com>
2025-05-06 22:25:20 -0300

    feat: Inserindo informações de contato (botões)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,46 +1,69 @@
+import { FaGithub } from 'react-icons/fa';
 import cv from '../../img/cv.jpg';
 // import teste from '../../img/teste.jpg';
 import './Home.css'; // (se quiser separar estilos)
 
 const Home = () => {
   return (
-    <div className="d-flex align-items-center justify-content-around flex-wrap text-center">
-      <div style={{ maxWidth: '600px' }}>
-        <h5>Desenvolvedor de Software</h5>
-        <p className="display-3 text-wrap font-weight-bold">
-          Olá, eu sou <br />
-          <span style={{ color: '#5856D6', fontWeight: 'bold' }}>
-            Guilherme Batista
-          </span>
-        </p>
-        <p
-          style={{
-            color: '#D2D2D3',
-            textAlign: 'justify',
-            paddingTop: '10px',
-          }}
-        >
-          Desenvolvedor de software com experiência no desenvolvimento de
-          soluções criativas e eficazes, tanto para front-end quanto para
-          back-end. Domino diversas tecnologias para criar aplicações ágeis,
-          seguras e de alta performance. Explore meu portfólio e veja como posso
-          contribuir com soluções sob medida para o seu projeto.
-        </p>
+    <>
+      <div className="d-flex align-items-center justify-content-around flex-wrap text-center">
+        <div style={{ maxWidth: '600px' }}>
+          <h5>Desenvolvedor de Software</h5>
+          <p className="display-3 text-wrap font-weight-bold">
+            Olá, eu sou <br />
+            <span style={{ color: '#5856D6', fontWeight: 'bold' }}>
+              Guilherme Batista
+            </span>
+          </p>
+          <p
+            style={{
+              color: '#D2D2D3',
+              textAlign: 'justify',
+              paddingTop: '10px',
+            }}
+          >
+            Desenvolvedor de software com experiência no desenvolvimento de
+            soluções criativas e eficazes, tanto para front-end quanto para
+            back-end. Domino diversas tecnologias para criar aplicações ágeis,
+            seguras e de alta performance. Explore meu portfólio e veja como
+            posso contribuir com soluções sob medida para o seu projeto.
+          </p>
+        </div>
+        <div>
+          <img
+            src={cv}
+            alt="Portfólio Guilherme Batista"
+            className="rounded-circle"
+            style={{
+              width: '350px',
+              height: '350px',
+              border: '5px solid #5856D6',
+              boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+            }}
+          />
+        </div>
       </div>
-      <div>
-        <img
-          src={cv}
-          alt="Portfólio Guilherme Batista"
-          className="rounded-circle"
-          style={{
-            width: '350px',
-            height: '350px',
-            border: '5px solid #5856D6',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
-          }}
+      <div
+        className="btn"
+        style={{
+          width: '50px',
+          height: '50px',
+          position: 'fixed',
+          bottom: '20px',
+          borderRadius: '50%',
+          right: '20px',
+          borderColor: '#5856D6',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      >
+        <FaGithub
+          style={{ width: '100px', height: '100px', background: 'none' }}
         />
       </div>
-    </div>
+    </>
   );
 };
 
