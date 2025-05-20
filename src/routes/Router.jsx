6f9bc05b866ab5751c174eb61c2bd41eddfa13dc
Guilherme Batista <capixth@gmail.com>
2025-05-19 21:20:37 -0300

    feat: Adicionando rota para o componente de contato

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from '../components/Home/Home.jsx'
+import { Route, Routes } from 'react-router';
+import Contact from '../components/Contato/Contact.jsx';
+import Home from '../components/Home/Home';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
-  )
-}
-export default AppRoutes
+  );
+};
+export default AppRoutes;
