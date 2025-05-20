b5f02010fcb6636c9a6c1bea5480ed05ca25d35f
Guilherme Batista <capixth@gmail.com>
2025-05-20 00:19:45 -0300

    refactor: Ajustando a formatação do componente Services para melhor legibilidade

--- src/components/Serviços/Services.jsx
+++ src/components/Serviços/Services.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-
 const Services = () => {
-    return (
-        <section>
-            <h2>Nossos Serviços</h2>
-            <ul>
-                <li>Desenvolvimento Web</li>
-                <li>Design Responsivo</li>
-                <li>Consultoria em TI</li>
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h2>Nossos Serviços</h2>
+      <ul>
+        <li>Desenvolvimento Web</li>
+        <li>Design Responsivo</li>
+        <li>Consultoria em TI</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Services;
