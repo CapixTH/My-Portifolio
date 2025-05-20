1eb28636ddbabf5922f3900a68c7c026c0f286c2
Guilherme Batista <capixth@gmail.com>
2025-05-19 21:20:44 -0300

    refactor: Organizando a estrutura do componente principal e ajustando a importação das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Nav from './components/NavBar/Nav.jsx';
-import Home from './components/Home/Home.jsx';
-import Contact from './components/Contato/Contact.jsx';
+// import Home from './components/Home/Home.jsx';
+// import Contact from './components/Contato/Contact.jsx';
+import AppRoutes from './routes/Router.jsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Nav />
-    {/* <div style={{ paddingTop: '50px' }}>
+    <div>
+      <Nav/>
+    </div>
+    <BrowserRouter>
+      {/* <div style={{ paddingTop: '50px' }}>
       <Home />
-    </div> */}
-    <Contact />
+      </div> */}
+      {/* <Contact /> */}
+      <AppRoutes />
+    </BrowserRouter>
   </StrictMode>
 );
