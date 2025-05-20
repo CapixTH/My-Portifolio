f190b37755d8189f951f27b2d11ee987dc627319
Guilherme Batista <capixth@gmail.com>
2025-05-19 21:36:46 -0300

    refactor: Removendo comentários e simplificando a estrutura do componente principal

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Nav from './components/NavBar/Nav.jsx';
-// import Home from './components/Home/Home.jsx';
-// import Contact from './components/Contato/Contact.jsx';
 import AppRoutes from './routes/Router.jsx';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='mb-5'>
-      <Nav/>
-    </div>
     <BrowserRouter>
-      {/* <div style={{ paddingTop: '50px' }}>
-      <Home />
-      </div> */}
-      {/* <Contact /> */}
+      <Nav />
       <AppRoutes />
     </BrowserRouter>
   </StrictMode>
