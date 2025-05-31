984ff05a53f1a40aed935e2f756ce38c9593efb9
Guilherme Batista <capixth@gmail.com>
2025-05-31 12:36:59 -0300

    refactor: Melhorando a estrutura do componente Nav e ajustando estilos de navegação

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,42 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@coreui/coreui/dist/css/coreui.min.css';
 import './Nav.css';
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className="d-flex justify-content-between align-items-center">
-        <div>
-          <a
-            href="Home"
-            className="fs-2"
-            style={{ color: '#fff', textDecoration: 'none' }}
-          >
-            Guilherme <span style={{ color: '#5856D6' }}>.</span>
-          </a>
-        </div>
-        <div className="d-inline-flex fs-4 ">
-          <a href="/" className="mx-2 nav-link-custom py-1 px-2">
+    <nav className="navbar navbar-expand-md container py-3">
+      <a
+        href="/"
+        className="fs-2 navbar-brand"
+        style={{ color: '#fff', textDecoration: 'none' }}
+      >
+        Guilherme <span style={{ color: '#5856D6' }}>.</span>
+      </a>
+
+      <button
+        className="navbar-toggler text-white border-0"
+        type="button"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+
+      <div
+        className={`collapse navbar-collapse justify-content-end ${
+          menuOpen ? 'show' : ''
+        }`}
+      >
+        <div className="navbar-nav text-center">
+          <a href="/" className="nav-link nav-link-custom mx-2 px-2">
             Home
           </a>
-          <a href="services" className="mx-2 nav-link-custom py-1 px-2">
+          <a href="services" className="nav-link nav-link-custom mx-2 px-2">
             Serviços
           </a>
-          <a href="resum" className="mx-2 nav-link-custom py-1 px-2">
+          <a href="resum" className="nav-link nav-link-custom mx-2 px-2">
             Resumo
           </a>
-          <a href="works" className="mx-2 nav-link-custom py-1 px-2">
+          <a href="works" className="nav-link nav-link-custom mx-2 px-2">
             Trabalhos
           </a>
-          <a href="contact" className="mx-2 nav-link-custom py-1 px-2">
+          <a href="contact" className="nav-link nav-link-custom mx-2 px-2">
             Contato
           </a>
-          <a href="teste" id="hire-me">
+          <a href="teste" id="hire-me" className="mx-2 mt-2 mt-md-0">
             Hire me
           </a>
         </div>
       </div>
-    </>
+    </nav>
   );
 };
 
