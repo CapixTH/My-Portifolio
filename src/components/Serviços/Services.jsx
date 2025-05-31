c85d0302d1a0b5c630064b9065bdf527cbd81d97
Guilherme Batista <capixth@gmail.com>
2025-05-31 12:42:30 -0300

    feat: Reestruturando o componente Services com novos cards de serviço e ícones

--- src/components/Serviços/Services.jsx
+++ src/components/Serviços/Services.jsx
@@ -1,13 +1,108 @@
+import { BsArrowDownRightCircleFill } from 'react-icons/bs';
+
 const Services = () => {
   return (
-    <section>
-      <h2>Nossos Serviços</h2>
-      <ul>
-        <li>Desenvolvimento Web</li>
-        <li>Design Responsivo</li>
-        <li>Consultoria em TI</li>
-      </ul>
-    </section>
+    <>
+      <div className="container pt-3">
+        <div className="row g-4">
+          <div className="col-md-6">
+            <div className="service-card border-bottom border-primary">
+              <div>
+                <div
+                  style={{
+                    fontSize: '3rem',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                  }}
+                >
+                  <div>01</div>
+                  <div>
+                    <BsArrowDownRightCircleFill />
+                  </div>
+                </div>
+                <h1>Desenvolvimento Web</h1>
+              </div>
+
+              <p className='text-secondary'>
+                Construção de sites responsivos e de alta performance com foco
+                na experiência do usuário e tecnologias modernas.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="service-card border-bottom border-primary">
+              <div
+                style={{
+                  fontSize: '3rem',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                }}
+              >
+                <div>02</div>
+                <div>
+                  <BsArrowDownRightCircleFill />
+                </div>
+              </div>
+              <h1>Design UI/UX</h1>
+              <p className='text-secondary'>
+                Criação de interfaces intuitivas e visualmente atraentes que
+                aumentam o engajamento do usuário.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 pt-3">
+            <div className="service-card">
+              <div
+                style={{
+                  fontSize: '3rem',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                }}
+              >
+                <div>03</div>
+                <div>
+                  <BsArrowDownRightCircleFill />
+                </div>
+              </div>
+              <h1>Inteligência de Negócios</h1>
+              <p className='text-secondary'>
+                Transformando dados em insights com análises avançadas e
+                visualizações eficazes.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 pt-3">
+            <div className="service-card">
+              <div
+                style={{
+                  fontSize: '3rem',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                }}
+              >
+                <div>04</div>
+                <div>
+                  <BsArrowDownRightCircleFill />
+                </div>
+              </div>
+              <h1>Suporte Técnico</h1>
+              <p className='text-secondary'>
+                Assistência técnica para resolver problemas de software e
+                hardware.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
