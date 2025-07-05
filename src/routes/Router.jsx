210503304c123c7428feb1b87d68990d30010447
Guilherme Batista <capixth@gmail.com>
2025-07-05 18:55:59 -0300

    feat: Adicionando rota para o componente Resum

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import Home from '../components/Home/Home';
 import Services from '../components/pages/Services.jsx';
 import Contact from '../components/pages/Contact.jsx';
+import Resum from '../components/pages/Resum.jsx';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
+      <Route path="/resum" element={<Resum />} />
     </Routes>
   );
 };
