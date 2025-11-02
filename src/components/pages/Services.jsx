925434467a7bee1a8c852e419787c266244365ab
Guilherme Batista <capixth@gmail.com>
2025-11-02 01:16:59 -0300

    feat: Refatorando estilos de cards e ajustando scroll global para melhor responsividade

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,72 +1,84 @@
 import { BsArrowDownRightCircleFill } from 'react-icons/bs';
+import { CContainer, CRow, CCol, CCard, CCardBody } from '@coreui/react';
 import '../Css/Services.css';
 
 const Services = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="service-card border-bottom border-primary border-2">
-            <div className="service-header">
-              <div>01</div>
-              <div className="service-icon">
-                <BsArrowDownRightCircleFill />
+    <CContainer className="container">
+      <CRow>
+        <CCol xs={12} md={6}>
+          <CCard className="service-card border-bottom border-primary border-2">
+            <CCardBody>
+              <div className="service-header">
+                <div>01</div>
+                <div className="service-icon">
+                  <BsArrowDownRightCircleFill />
+                </div>
               </div>
-            </div>
-            <h1 className="service-title">Desenvolvimento Web</h1>
-            <p className="service-description">
-              Construção de sites responsivos e de alta performance com foco na
-              experiência do usuário e tecnologias modernas.
-            </p>
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="service-card border-bottom border-primary border-2">
-            <div className="service-header">
-              <div>02</div>
-              <div className="service-icon">
-                <BsArrowDownRightCircleFill />
+              <h1 className="service-title">Desenvolvimento Web</h1>
+              <p className="service-description">
+                Construção de sites responsivos e de alta performance com foco
+                na experiência do usuário e tecnologias modernas.
+              </p>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol xs={12} md={6}>
+          <CCard className="service-card border-bottom border-primary border-2">
+            <CCardBody>
+              <div className="service-header">
+                <div>02</div>
+                <div className="service-icon">
+                  <BsArrowDownRightCircleFill />
+                </div>
               </div>
-            </div>
-            <h1 className="service-title">Design UI/UX</h1>
-            <p className="service-description">
-              Criação de interfaces intuitivas e visualmente atraentes que
-              aumentam o engajamento do usuário.
-            </p>
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="service-card border-bottom border-primary border-2">
-            <div className="service-header">
-              <div>03</div>
-              <div className="service-icon">
-                <BsArrowDownRightCircleFill />
+              <h1 className="service-title">Design UI/UX</h1>
+              <p className="service-description">
+                Criação de interfaces intuitivas e visualmente atraentes que
+                aumentam o engajamento do usuário.
+              </p>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol xs={12} md={6}>
+          <CCard className="service-card border-bottom border-primary border-2">
+            <CCardBody>
+              <div className="service-header">
+                <div>03</div>
+                <div className="service-icon">
+                  <BsArrowDownRightCircleFill />
+                </div>
               </div>
-            </div>
-            <h1 className="service-title">Inteligência de Negócios</h1>
-            <p className="service-description">
-              Transformando dados em insights com análises avançadas e
-              visualizações eficazes.
-            </p>
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="service-card border-bottom border-primary border-2">
-            <div className="service-header">
-              <div>04</div>
-              <div className="service-icon">
-                <BsArrowDownRightCircleFill />
+              <h1 className="service-title">Inteligência de Negócios</h1>
+              <p className="service-description">
+                Transformando dados em insights com análises avançadas e
+                visualizações eficazes.
+              </p>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol xs={12} md={6}>
+          <CCard className="service-card border-bottom border-primary border-2">
+            <CCardBody>
+              <div className="service-header">
+                <div>04</div>
+                <div className="service-icon">
+                  <BsArrowDownRightCircleFill />
+                </div>
               </div>
-            </div>
-            <h1 className="service-title">Suporte Técnico</h1>
-            <p className="service-description">
-              Assistência técnica para resolver problemas de software e
-              hardware.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+              <h1 className="service-title">Suporte Técnico</h1>
+              <p className="service-description">
+                Assistência técnica para resolver problemas de software e
+                hardware.
+              </p>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
   );
 };
 
